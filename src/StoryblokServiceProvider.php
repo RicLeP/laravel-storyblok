b6850bccf124f0e82bedcd24cdda80c60e9bb5d2
Ric Le Poidevin <ric@wearebwi.com>
2020-08-28 14:52:17 +0100

    disable routes for the moment

--- src/StoryblokServiceProvider.php
+++ src/StoryblokServiceProvider.php
@@ -12,7 +12,7 @@ class StoryblokServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-       $this->loadRoutesFrom(__DIR__.'/routes/api.php');
+       //$this->loadRoutesFrom(__DIR__.'/routes/api.php');
        $this->loadRoutesFrom(__DIR__.'/routes/routes.php');
 
 		$this->loadViewsFrom(__DIR__.'/resources/views', 'laravel-storyblok');
