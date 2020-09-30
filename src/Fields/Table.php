3944bfd80f3f7c3c897d48f1dbfd6aa3010427f4
Ric Le Poidevin <ric@wearebwi.com>
2020-09-30 15:39:14 +0100

    Tables can have $cssClass applied

--- src/Fields/Table.php
+++ src/Fields/Table.php
@@ -4,17 +4,28 @@
 namespace Riclep\Storyblok\Fields;
 
 
+use Illuminate\Support\Arr;
 use Riclep\Storyblok\Field;
 
 class Table extends Field
 {
+	/**
+	 * @var string a class to apply to the <table> tag
+	 */
+	protected $cssClass;
+
+	/**
+	 * @var array|int the column numbers to convert to headers
+	 */
+	protected $headerColumns;
+
 	public function __toString()
 	{
 		return $this->toHtml($this->content);
 	}
 
 	protected function toHtml($table) {
-		$html = '<table><thead><tr>';
+		$html = '<table ' . ($this->cssClass ? 'class="' . $this->cssClass . '"' : null) . '><thead><tr>';
 
 		foreach ($table['thead'] as $header) {
 			$html .= '<th>' . $header['value'] . '</th>';
@@ -25,8 +36,12 @@ class Table extends Field
 		foreach ($table['tbody'] as $row) {
 			$html .= '<tr>';
 
-			foreach ($row['body'] as $cell) {
-				$html .= '<td>' . $cell['value'] . '</td>';
+			foreach ($row['body'] as $column => $cell) {
+				if ($this->headerColumns && in_array(($column + 1), Arr::wrap($this->headerColumns))) {
+					$html .= '<th>' . $cell['value'] . '</th>';
+				} else {
+					$html .= '<td>' . $cell['value'] . '</td>';
+				}
 			}
 
 			$html .= '</tr>';
