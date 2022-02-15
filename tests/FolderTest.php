a01813546de73e6c56e14f9b007712f5ed03b460
Richard Le Poidevin <ric@sirric.co.uk>
2022-02-15 12:02:15 +0000

    Better folders! WIP

---
+++ tests/FolderTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Riclep\Storyblok\Tests;
+
+
+use Illuminate\Support\Collection;
+use Riclep\Storyblok\Page;
+use Riclep\Storyblok\Tests\Fixtures\Folders\Folder;
+use Riclep\Storyblok\Tests\Fixtures\Folders\EmptyFolder;
+
+class FolderTest extends TestCase
+{
+	/** @test */
+	public function can_get_total_stories() {
+		$folder = new Folder();
+		$folder->read();
+
+		$folder2 = new EmptyFolder();
+		$folder2->read();
+
+		$this->assertEquals(15, $folder->totalStories);
+		$this->assertEquals(0, $folder2->totalStories);
+	}
+
+	/** @test */
+	public function can_get_total_stories_for_this_page() {
+		$folder = new Folder();
+		$folder->read();
+
+		$this->assertEquals(4, $folder->count());
+	}
+
+	/** @test */
+	public function will_return_zero_stories_for_empty_folder() {
+		$folder = new EmptyFolder();
+		$folder->read();
+
+		$this->assertEquals(0, $folder->count());
+	}
+
+	/** @test */
+	public function can_get_folder_stories() {
+		$folder = new Folder();
+		$folder->read();
+
+		$this->assertInstanceOf(Collection::class, $folder->stories);
+		$this->assertInstanceOf(Page::class, $folder->stories[0]);
+	}
+
+	/** @test */
+	public function can_paginate_stories() {
+		$folder = new Folder();
+		$folder->read();
+
+		dd($folder->paginate()->links());
+	}
+}
+
